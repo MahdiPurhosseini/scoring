e27c560102aecd36b746f94957064e20e23bbc11
mahdi purHosseini <mahdip9811@gmail.com>
2022-09-06 14:02:33 +0430

    fix lang

--- app/Http/Controllers/Api/UserDeviceAPIController.php
+++ app/Http/Controllers/Api/UserDeviceAPIController.php
@@ -8,9 +8,6 @@ use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Response;
 
-/**
- * Class UserDeviceAPIController
- */
 class UserDeviceAPIController extends Controller
 {
     /** @var UserDeviceRepository */
